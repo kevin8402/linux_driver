1830785a3b135aebd1c7e706e44e24798222d43b
kevin <kevin8402@126.com>
2017-02-06 03:01:51 -0800

    Add proc fs entry

--- misc_device/mydev.c
+++ misc_device/mydev.c
@@ -5,8 +5,17 @@
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
+
+struct mydevice{
+	struct miscdevice miscdev;
+	int value;
+};
+
+static struct mydevice mydev;
 
 static int mydev_open(struct inode *inode, struct file *file)
 {
@@ -18,30 +27,56 @@ static int mydev_release(struct inode *inode, struct file *file)
 	return 0;
 }
 
-struct file_operations mydev_fops = {
+static struct file_operations mydev_fops = {
 	.owner = THIS_MODULE,
 	.open = mydev_open,
 	.release = mydev_release,
 };
 
-static struct miscdevice mydev =
+static ssize_t mydev_proc_read(struct file *file, char __user *buf,
+		size_t count, loff_t *ppos)
 {
-	.minor		= MISC_DYNAMIC_MINOR,
-	.name		= "mydev",
-	.fops		= &mydev_fops,
+	char *str = "value=100\n";
+	size_t len = 0;
+
+	if (*ppos == 0) {
+		len = strlen(str) + 1;
+		copy_to_user(buf, str, len);
+		*ppos += len;
+	} else {
+		len = 0;
+	}
+
+	printk(KERN_DEBUG "proc, len=%d\n", len);
+
+	return len;
+}
+
+static struct file_operations mydev_proc_fops = {
+	.owner = THIS_MODULE,
+	.read = mydev_proc_read,
 };
 
 static int mydev_init(void)
 {
 	int ret;
+	struct miscdevice *miscdev;
 
-	printk(KERN_ALERT "mydev_init\n");
+	printk(KERN_DEBUG "mydev init\n");
 	
-	ret = misc_register(&mydev);
+	miscdev = &mydev.miscdev;
+	miscdev->minor = MISC_DYNAMIC_MINOR;
+	miscdev->name = "mydev";
+	miscdev->fops = &mydev_fops;
+	mydev.value = 100;
+
+	ret = misc_register(miscdev);
 	if (ret < 0) {
-		printk(KERN_ALERT "misc register failed\n");
+		printk(KERN_DEBUG "misc register failed\n");
 		return -1;
 	}
+
+	proc_create("mydev", 0, NULL, &mydev_proc_fops);
 
 
 	return 0;
@@ -49,9 +84,11 @@ static int mydev_init(void)
 
 static void mydev_exit(void)
 {
-	misc_deregister(&mydev);
+	remove_proc_entry("mydev", NULL);
 
-	printk(KERN_ALERT "exit\n");
+	misc_deregister(&mydev.miscdev);
+
+	printk(KERN_DEBUG "mydev exit\n");
 }
 
 module_init(mydev_init);
